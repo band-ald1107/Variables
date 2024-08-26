3f4322f78dd6e60b0e7070db289d0fbd7044a83e
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-08-26 19:41:24 +0800

    Add files via upload

---
+++ Home work (Variables).py
@@ -0,0 +1,9 @@
+number_of_completed_tasks = 12
+print(number_of_completed_tasks)
+number_of_hours_spent = 1.5
+print(number_of_hours_spent)
+course_title = 'Python'
+print(course_title)
+average_execution_time = number_of_hours_spent  / number_of_completed_tasks
+print( number_of_hours_spent /  number_of_completed_tasks)
+print('Курс:', course_title ,   'всего задач:', number_of_completed_tasks , 'затрачено часов:', number_of_hours_spent, 'среднее время выполнения:', average_execution_time )
